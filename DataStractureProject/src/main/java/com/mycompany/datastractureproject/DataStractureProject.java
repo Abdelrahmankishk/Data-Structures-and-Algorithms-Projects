9ffe45eb810ce40ff0935475b98a38bf03f09af6
Abdelrahman Keshk <89317207+Abdelrahmankishk@users.noreply.github.com>
2023-06-01 15:32:01 +0300

    Update DataStractureProject.java

--- DataStractureProject/src/main/java/com/mycompany/datastractureproject/DataStractureProject.java
+++ DataStractureProject/src/main/java/com/mycompany/datastractureproject/DataStractureProject.java
@@ -2,7 +2,7 @@ package com.mycompany.datastractureproject;
 import java.util.Scanner;
 public class DataStractureProject {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
         System.out.print("Input: ");
         String infix = x.nextLine();
@@ -17,23 +17,23 @@ public class DataStractureProject {
             char c = infix.charAt(i);
             
             if (Character.isDigit(c)) {
-                postFix += c;
+                postFix += c + " ";
             } else if (c == '(') {
                 myStack.push(c);
             } else if (c == ')') {
                 while (myStack.isEmpty() == false && myStack.top() != '(') {
-                    postFix += myStack.pop();
+                    postFix += myStack.pop() + " ";
                 }
                 myStack.pop(); 
             } else {
                 while (myStack.isEmpty() == false && precedence(c) <= precedence(myStack.top())) {
-                    postFix += myStack.pop();
+                    postFix += myStack.pop() + " ";
                 }
                 myStack.push(c);
             }
         }
          while (myStack.isEmpty()== false) {
-            postFix += myStack.pop();
+            postFix += myStack.pop() + " ";
         }
         
         return postFix;
