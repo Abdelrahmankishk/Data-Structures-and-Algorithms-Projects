bc6fe0ec7d9bf75749c115c5ec468b1f2d03a015
Abdelrahman Keshk <89317207+Abdelrahmankishk@users.noreply.github.com>
2023-06-01 15:24:35 +0300

    Add files via upload
    A Project with Stack Class that read expression written in infix notation and convert it to postfix

---
+++ DataStractureProject/src/main/java/com/mycompany/datastractureproject/Stack.java
@@ -0,0 +1,52 @@
+package com.mycompany.datastractureproject;
+
+public class Stack {
+    private class Node{
+        char data;
+        Node next;        
+    }
+    Node top;
+    
+    public boolean isEmpty(){
+        if(top == null){
+            return true;
+        }
+        else{return false;}
+    }
+    
+    public void push(char x){
+        Node temp = new Node();
+        if (temp == null){
+            System.out.println("Error: Stack Underflow");
+            return;
+        }
+        temp.data = x;
+        temp.next = top;
+        top = temp;
+    }
+    
+    public char top(){
+        if(isEmpty()== true){
+            System.out.println("Stack is empty");
+            return 'N';
+        }
+        else{
+            return top.data;
+        }
+    }
+    
+    public char pop(){
+        Node temp = new Node();
+        if (top == null){
+            System.out.println("Stack Underflow");
+            return 'N';
+        }
+        else{
+            temp =top;   
+            top = top.next;
+            return temp.data;
+        }
+    }
+    
+    
+}
